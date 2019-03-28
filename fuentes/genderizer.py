623b804286e11f9d2bd4bca9e53e5d06dd26642d
fvillena <villenafabian@gmail.com>
2019-03-27 22:57:34 -0300

    initial commit

--- fuentes/genderizer.py
+++ fuentes/genderizer.py
@@ -1,40 +1,14 @@
-
-# coding: utf-8
-
-# In[20]:
-
-
 import os
 import re
-
-
-# In[21]:
-
-
 words = []
 for file in os.listdir():
     if file.endswith(".txt") and not file.startswith('dict'):
         file = open(file, "r", encoding="utf-8")
         for line in file:
             words.append(line.rstrip())
-
-
-# In[22]:
-
-
 cleanWords = sorted(set([re.sub('[0-9- ]', '', word) for word in words]))
-
-
-# In[23]:
-
-
 withComma = [word for word in cleanWords if ',' in word]
 withoutComma = [word for word in cleanWords if ',' not in word]
-
-
-# In[24]:
-
-
 genderized = []
 for word in withComma:
     male = re.search(r'(\w*),(\w*)',word)[1]
@@ -64,17 +38,6 @@ for word in withComma:
     except:
         genderized.append(male)
         print(male)
-
-
-# In[25]:
-
-
 words = sorted(set(withoutComma + genderized))
-
-
-# In[26]:
-
-
 with open('../diccionario-rae-completo.txt', mode='wt', encoding='utf-8') as file:
     file.write('\n'.join(words))
-
